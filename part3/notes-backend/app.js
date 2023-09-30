620f2702bb53c6efe7fbc6969eef31fab3f4de0c
dtchoi <choiboy517@gmail.com>
2023-09-29 18:48:54 -0600

    user administration

--- part3/notes-backend/app.js
+++ part3/notes-backend/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+const usersRouter = require('./controllers/users')
 const notesRouter = require('./controllers/notes')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -20,12 +21,12 @@ mongoose.connect(config.MONGODB_URI)
         logger.error('error connecting to MongoDB:', error.message)
     })
 
-
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/users', usersRouter)
 app.use('/api/notes', notesRouter)
 
 app.use(middleware.unknownEndpoint)
