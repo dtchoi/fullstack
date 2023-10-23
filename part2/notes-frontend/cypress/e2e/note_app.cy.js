5c1bb18454585aa3b2b2646536be677483a57359
dtchoi <choiboy517@gmail.com>
2023-10-23 13:58:54 -0600

    E2E testing - cypress, login, new note, reset

---
+++ part2/notes-frontend/cypress/e2e/note_app.cy.js
@@ -0,0 +1,37 @@
+describe('Note app', function() {
+  beforeEach(function() {
+    cy.visit('http://localhost:5173')
+  })
+
+  it('front page can be opened', function() {
+    cy.contains('Notes')
+    cy.contains('Note app, Department of Computer Science, University of Helsinki 2023')
+  })
+
+  it('user can log in', function() {
+    cy.contains('login').click()
+    cy.get('#username').type('dtchoi')
+    cy.get('#password').type('thisismypasswordpleasedontcrack')
+    cy.get('#login-button').click()
+
+    cy.contains('Daniel Choi logged in')
+  })
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.contains('login').click()
+      cy.get('#username').type('dtchoi')
+      cy.get('#password').type('thisismypasswordpleasedontcrack')
+      cy.get('#login-button').click()
+    })
+
+    it('a new note can be created', function() {
+      cy.contains('new note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
+    })
+  })
+
+
+})
