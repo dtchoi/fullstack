0ef7404ebf2e1d223d846f354e95e390e84c13da
dtchoi <choiboy517@gmail.com>
2023-10-31 19:21:53 -0600

    combined reducers, filter notes

--- part6/redux-notes/src/App.jsx
+++ part6/redux-notes/src/App.jsx
@@ -1,11 +1,17 @@
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
+import VisibilityFilter from './components/VisibilityFilter'
 
 const App = () => {
+
+  const filteredSelected = (value) => {
+    console.log(value)
+  }
 
   return(
     <div>
       <NewNote />
+      <VisibilityFilter />
       <Notes />
     </div>
   )
